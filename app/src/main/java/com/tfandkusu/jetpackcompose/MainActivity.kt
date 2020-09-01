a884cc8733fecf51f819775ed1f6ee97226397ef
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-09-02 00:27:53 +0900

    Initial commit

---
+++ app/src/main/java/com/tfandkusu/jetpackcompose/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tfandkusu.jetpackcompose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.ui.platform.setContent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello World")
+        }
+    }
+}
