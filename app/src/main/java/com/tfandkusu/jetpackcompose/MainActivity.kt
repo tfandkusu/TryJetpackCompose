f4f4dda5ad4220d815a4f9ed2bdaf0a60c421295
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-09-02 00:34:19 +0900

    Previewまで作成

--- app/src/main/java/com/tfandkusu/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/tfandkusu/jetpackcompose/MainActivity.kt
@@ -3,13 +3,26 @@ package com.tfandkusu.jetpackcompose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World")
+            PreviewGreeting()
         }
+    }
+
+    @Composable
+    @Preview
+    fun PreviewGreeting() {
+        Greeting(name = "Android")
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name !")
     }
 }
